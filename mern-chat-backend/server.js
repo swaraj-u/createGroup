038eb222647d60371ca188f377c93c55725cb144
swaraj-u <swarajrupadhyay@gmail.com>
2024-09-16 16:31:17 +0530

    added frontend url

--- mern-chat-backend/server.js
+++ mern-chat-backend/server.js
@@ -19,7 +19,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-app-frontend-pearl.vercel.app',
     methods: ['GET', 'POST']
   }
 })
